11a1acc5f271df1223f55e35bda0a53e15018909
Malachi Swedberg <malachiswedberg@gmail.com>
2019-03-12 22:58:08 -0500

    Second Commit

--- src/HealthyPerson.java
+++ src/HealthyPerson.java
@@ -7,15 +7,30 @@ public class HealthyPerson extends Person{
 
 	private String reason;
 	
+	/**
+	 * 
+	 * @param name of patient
+	 * @param age of patient
+	 * @param reason for coming to the Hospital
+	 */
 	public HealthyPerson(String name, int age, String reason) {
 		super(name, age);
 		this.reason = reason;
 	}
 	
+	/**
+	 * 
+	 * @return reason for the patient instance
+	 */
 	public String getReason() {
 		return reason;
 	}
 	
+	/**
+	 * @return 1 - this comes before p
+	 * 		   -1 - this comes after p
+	 * 		   0 - names are alphebitically the same, or if p is not an instance of HealthyPerson
+	 */
 	protected int compareToImpl(Person p) {
 		if (!(p instanceof HealthyPerson)) {
 			return 0;
@@ -31,6 +46,9 @@ public class HealthyPerson extends Person{
 		}
 	}
 	
+	/**
+	 * @return the super's to string of the patient, followed by their reasoning for coming
+	 */
 	public String toString() {
 		return String.format("%s In for %s", super.toString(), reason);
 	}
--- src/SickPerson.java
+++ src/SickPerson.java
@@ -5,17 +5,30 @@
  */
 public class SickPerson extends Person{
 
+	//Severity of the patients condition
 	private int severity;
 	
+	/**
+	 * @param name of the patient
+	 * @param severity of the patient's condition
+	 * @param age of the patient
+	 */
 	public SickPerson(String name, int severity, int age) {
 		super(name, age);
 		this.severity = severity;
 	}
 	
+	//getter for the Severity of a SickPerson instance
 	public int getSeverity() {
 		return severity;
 	}
 	
+	/**
+	 * @return
+	 * 	negitive if p's severity is less than the given instance
+	 * 	Positive if p's severity is greater than the given instance
+	 * 	0 if the severity is equivilant, or if p is not an instance of SickPerson
+	 */
 	@Override
 	protected int compareToImpl(Person p) {
 		if (!(p instanceof SickPerson)) {
@@ -25,6 +38,9 @@ public class SickPerson extends Person{
 		return o.severity - severity;
 	}
 	
+	/**
+	 * @return the supers toString followed by the addition of severity
+	 */
 	public String toString() {
 		return String.format("%s Severity level %d", super.toString(), severity);
 	}
