86d64ebb0a9093011f0e4aa529508a58bd20b23c
Malachi Swedberg <malachiswedberg@gmail.com>
2019-03-12 23:01:30 -0500

    Third Commit

--- src/StackHospital.java
+++ src/StackHospital.java
@@ -10,14 +10,23 @@ public class StackHospital<PatientType> extends Hospital{
 	
 	private ArrayList<PatientType> patients;
 	
+	/**
+	 * initializes the arrayList for the Hospital
+	 */
 	public StackHospital() {
 		this.patients = new ArrayList<PatientType>(25);
 	}
 
+	/**
+	 * @return the next patient to be treated, which is the last in the ArrayList
+	 */
 	public PatientType nextPatient() {
 		return patients.get(patients.size() - 1);
 	}
 	
+	/**
+	 * @return the next patient to be treated, while also resaving a new ArrayList without the treated patient.
+	 */
 	public PatientType treatNextPatient() {
 		
 		ArrayList<PatientType> temp = new ArrayList<PatientType>(patients.size());
@@ -30,15 +39,23 @@ public class StackHospital<PatientType> extends Hospital{
 		return temp2;
 	}
 	
-	
+	/**
+	 * @return the size of the patients arrayList
+	 */
 	public int numPatients() {
 		return patients.size();
 	}
 	
+	/**
+	 * @return the type of Hospital the object is
+	 */
 	public String hospitalType() {
 		return "StackHospital";
 	}
 	
+	/**
+	 * Prints a toString of all patients found in the Hospital, simply concatonated onto one another
+	 */
 	public String allPatientInfo() {
 		String result = "";
 		for (int i = 0; i < patients.size(); ++i) {
@@ -47,6 +64,9 @@ public class StackHospital<PatientType> extends Hospital{
 		return result;
 	}
 
+	/**
+	 * Adds a patient to the ArrayList
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void addPatient(Object patient) {
